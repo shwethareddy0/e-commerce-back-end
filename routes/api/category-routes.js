b6740e9f75d8c50c91a60201110ad9ab7c9beb64
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-11-04 01:58:38 -0700

    add Get by id route to categories and tags

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,7 +18,7 @@ router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk(re.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
     });
     if (!categoryData) {
