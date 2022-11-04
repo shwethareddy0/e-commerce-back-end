411b23a0fa73184aee22224bc458796ff2a0cbd9
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-11-04 00:32:50 -0700

    add the GET routes for the product

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+
 const routes = require("./routes");
 // import sequelize connection
 const sequelize = require("./config/connection");
 
 const app = express();
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -12,6 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
