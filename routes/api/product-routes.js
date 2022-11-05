59cfd61f337edb60f0123de3fc8cfb2733272d9b
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-11-04 18:46:49 -0700

    update GET routes for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,9 @@ router.get("/", async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findAll();
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }],
+    });
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
@@ -28,7 +30,6 @@ router.get("/:id", async (req, res) => {
       res.status(404).json({ message: "No product found with this id!" });
       return;
     }
-
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
